079f5cc5f2089f3b89baecf29201aa6ca8a9fa7d
alyssonwolfpoet <49415479+alyssonwolfpoet@users.noreply.github.com>
2020-11-09 21:14:19 -0300

    Update Alyssonlastversion.sh

--- Alyssonlastversion.sh
+++ Alyssonlastversion.sh
@@ -1026,9 +1026,9 @@ sleep 5
     echo "================"
     wget --quiet -O - https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
     sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-    sudo apt install pgadmin4
-    sudo apt install pgadmin4-desktop
-    sudo apt install pgadmin4-web 
+    sudo apt install pgadmin4 -y
+    sudo apt install pgadmin4-desktop -y
+    sudo apt install pgadmin4-web -y
   }
   #19º ruby
   ruby(){
