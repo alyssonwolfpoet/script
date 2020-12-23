374b534a04b02e6251fbbf14a3a77a4a824524e1
alyssonwolfpoet <49415479+alyssonwolfpoet@users.noreply.github.com>
2020-12-22 21:28:14 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -500,16 +500,11 @@ sleep 5
 	    echo "*---php---*"
 	    echo "==========="
 		#sudo apt install php7.4-cli
-		wget -c https://www.php.net/distributions/php-8.0.0.tar.gz -O php-8.0.0.tar.gz
-		sudo tar -zxvf php-8.0.0.tar.gz
-		cd php-8.0.0
-		sudo apt install -y pkg-config build-essential autoconf bison re2c libxml2-dev libsqlite3-dev
-		sudo ./buildconf
-		sudo ./configure
-		sudo make
-		sudo make install
-		cd ..
-		sudo rm -rf php-8.0.0
+		sudo apt install software-properties-common -y
+		sudo add-apt-repository ppa:ondrej/php -y
+		sudo apt update
+		sudo apt install php8.0 libapache2-mod-php8.0 -y
+		sudo apt install php8.0-mysql php8.0-gd -y
 	}
 	#27 phpmyadmin
 	phpmyadmin(){
@@ -590,14 +585,10 @@ sleep 5
     echo "==================================="
     echo "*---         python            ---*"
     echo "==================================="
-    wget -c https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz -O Python-3.9.1.tgz
-	sudo tar -zxvf Python-3.9.1.tgz
-	cd Python-3.9.1
-	sudo ./configure
-	sudo make
-	sudo make install
-	cd ..
-	sudo rm -rf Python-3.9.1
+    sudo apt update
+	sudo apt install software-properties-common -y
+	sudo add-apt-repository ppa:deadsnakes/ppa -y
+	sudo apt install python3.9 -y
     echo -e "\n"
     echo "==================================="
     echo "*---         tkinter3            ---*"
