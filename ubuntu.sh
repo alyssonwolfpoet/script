0ea26281add84e13f59105e95e6d686667a270f3
alyssonwolfpoet <49415479+alyssonwolfpoet@users.noreply.github.com>
2020-12-29 15:52:46 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -540,6 +540,7 @@ sleep 5
 	  	wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
 		sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
 		sudo apt-get update
+		sudo apt install github-desktop -y
 	}
 	#11 gnome_panel
 	gnome_panel(){
