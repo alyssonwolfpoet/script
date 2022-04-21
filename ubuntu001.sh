07e2debf574dddb83b082faebec981c9abab5d22
alyssonwolfpoet <49415479+alyssonwolfpoet@users.noreply.github.com>
2022-04-21 18:51:17 -0300

    Update ubuntu001.sh

--- ubuntu001.sh
+++ ubuntu001.sh
@@ -8,7 +8,7 @@ echo -e "
 ''''''''''''''''''''''''''''''''''''''''''
 'Programa:Script de Programas            '
 'Desenvolvedor:Alysson Sousa            ''
-'Data de última modificação: 18/04/2022'''
+'Data de última modificação: 21/04/2022'''
 ''''''''''''''''''''''''''''''''''''''''''
 "
 sleep 5
