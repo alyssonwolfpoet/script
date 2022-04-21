dafbb854d4f6814e45e6cfb6dfa82ea864b70d6a
alyssonwolfpoet <49415479+alyssonwolfpoet@users.noreply.github.com>
2022-04-21 18:56:17 -0300

    Update AlyssonInstall001.sh

--- AlyssonInstall001.sh
+++ AlyssonInstall001.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #autor: Alysson Sousa
-#data: 18/04/2022
+#data: 21/04/2022
 
 #Créditos
 clear
@@ -8,7 +8,7 @@ echo -e "
 ''''''''''''''''''''''''''''''''''''''''''
 'Programa:Script de Programas            '
 'Desenvolvedor:Alysson Sousa            ''
-'Data de última modificação: 18/04/2022'''
+'Data de última modificação: 21/04/2022'''
 ''''''''''''''''''''''''''''''''''''''''''
 "
 sleep 5
@@ -17,8 +17,8 @@ echo -e "
 '      Dowload da última versão      '
 ''''''''''''''''''''''''''''''''''''''"
 wget -c https://raw.githubusercontent.com/alyssonwolfpoet/script/master/snap.sh -O snap.sh
-wget -c https://raw.githubusercontent.com/alyssonwolfpoet/script/master/ubuntu001.sh -O Alyssonlastversion.sh
-sudo chmod +x Alyssonlastversion.sh
+wget -c https://raw.githubusercontent.com/alyssonwolfpoet/script/master/ubuntu001.sh -O ubuntu001.sh
+sudo chmod +x ubuntu001.sh
 sudo chmod +x snap.sh
 
 echo -e "
@@ -36,9 +36,9 @@ echo -e "1"
 sleep 1
 
 sudo ./snap.sh
-sudo ./Alyssonlastversion.sh
-sudo rm Alyssonlastversion.sh
-#sudo rm AlyssonInstall.sh
+sudo ./ubuntu001.sh
+#sudo rm Alyssonlastversion.sh
+sudo rm AlyssonInstall001.sh
 
 clear
 echo -e "
