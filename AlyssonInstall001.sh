aa889075f8f86cb3b0428686cf8208ff02fdeab9
alyssonwolfpoet <49415479+alyssonwolfpoet@users.noreply.github.com>
2022-06-27 19:09:39 -0300

    Update AlyssonInstall001.sh

--- AlyssonInstall001.sh
+++ AlyssonInstall001.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #autor: Alysson Sousa
-#data: 18/06/2022
+#data: 27/06/2022
 
 #Créditos
 clear
@@ -8,7 +8,7 @@ echo -e "
 ''''''''''''''''''''''''''''''''''''''''''
 'Programa:Script de Programas            '
 'Desenvolvedor:Alysson Sousa            ''
-'Data de última modificação: 18/06/2022'''
+'Data de última modificação: 27/06/2022'''
 ''''''''''''''''''''''''''''''''''''''''''
 "
 sleep 5
