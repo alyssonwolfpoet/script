058b18eac6497be29ab016416137219fa1644a8b
Alysson <alyssos@hotmail.com>
2023-09-23 20:22:30 -0300

    postgress update

--- ubuntu001.sh
+++ ubuntu001.sh
@@ -851,8 +851,7 @@ sleep 5
 	    echo "================"
 	    echo "*---pgadmin4---*"
 	    echo "================"
-	    #wget --quiet -O - https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
-	    curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+	    wget --quiet -O - https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 	    sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 	    sudo apt install pgadmin4 -y
 	    sudo apt install pgadmin4-desktop -y
