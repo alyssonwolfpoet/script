b955b8707cecf19dd4d62b743e3f8e30efad6e4b
Alysson <alyssos@hotmail.com>
2023-09-24 12:05:45 -0300

    remoç~cao do postgress versao web

--- ubuntu001.sh
+++ ubuntu001.sh
@@ -852,10 +852,10 @@ sleep 5
 	    echo "================"
 	    wget --quiet -O - https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 	    sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-	    sudo apt install pgadmin4 -y
+	    #sudo apt install pgadmin4 -y
 	    sudo apt install pgadmin4-desktop -y
-	    sudo apt install pgadmin4-web -y
-	    sudo /usr/pgadmin4/bin/setup-web.sh
+	    #sudo apt install pgadmin4-web -y
+	    #sudo /usr/pgadmin4/bin/setup-web.sh
 	}
 	#33 project_libre
 	project_libre(){
