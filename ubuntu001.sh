4fc5180f67acc6ff0311d98f1b89a3422728d34d
Alysson <alyssos@hotmail.com>
2023-11-07 15:07:26 -0300

    :heavy_check_mark: feat: date update

--- ubuntu001.sh
+++ ubuntu001.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #autor: Alysson Sousa
-#data: 19/10/2023
+#data: 07/11/2023
 
 #Créditos
 clear
@@ -8,7 +8,7 @@ echo -e "
 ''''''''''''''''''''''''''''''''''''''''''
 'Programa:Script de Programas            '
 'Desenvolvedor:Alysson Sousa            ''
-'Data de última modificação: 19/10/2023'''
+'Data de última modificação: 07/11/2023'''
 ''''''''''''''''''''''''''''''''''''''''''
 "
 sleep 5
