523286833880c58ed769bbf04b64463e262ca78d
Alysson <alyssos@hotmail.com>
2024-04-03 08:09:24 -0300

    :sparkles: feat: Android studio local use

---
+++ studio_local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 -y
+
+wget -c https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.2.1.23/android-studio-2023.2.1.23-linux.tar.gz -O android-studio.tar.gz
+tar -zxvf android-studio.tar.gz
+
+rm android-studio.tar.gz
+
+echo -e "[Desktop Entry]\nEncoding=UTF-8\nVersion=1.0\nName=Android Studio Dolphin\nComment=Android Studio Iguana\nExec=/home/$USER/android-studio/bin/studio.sh\nIcon=/home/$USER/android-studio/bin/studio.png\nType=Application\nTerminal=false\nStartupNotify=true\nCategories=Application" |tee /home/$USER/.local/share/applications/android-studio.desktop
+chmod +x /.local/share/applications/android-studio.desktop
+rm studio.sh
+
